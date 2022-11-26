1782a23062c07c59d3603c5deb1942edb1d20284
AmbikaNagabushan <ambika.n@zucisystems.com>
2022-11-26 15:15:57 +0530

    react-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,70 @@
 import { Component } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'AdroInfoTech';
+  form: FormGroup = new FormGroup({
+    name: new FormControl(''),
+    dob: new FormControl(''),
+    phone: new FormControl(''),
+    email: new FormControl(''),
+  });
+  //to avoid future dates added max date to the date picker
+  maxDate = this.formatDate(new Date());
+
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(5)]],
+      dob: ['', Validators.required],
+      phone: [
+        '',
+        [Validators.required, Validators.pattern('^((\\+91-?)|0)?[0-9]{10}$')],
+      ],
+      email: ['', [Validators.required, Validators.email]],
+    });
+  }
+  padTo2Digits(num: Number) {
+    return num.toString().padStart(2, '0');
+  }
+  //to format the current date and time
+  formatDate(date: Date) {
+    return [
+      date.getFullYear(),
+      this.padTo2Digits(date.getMonth() + 1),
+      this.padTo2Digits(date.getDate()),
+    ].join('-');
+  }
+
+  get functionControl(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    console.log(JSON.stringify(this.form.value, null, 2));
+  }
+
+  onCancel(): void {
+    this.submitted = false;
+    this.form.reset();
+  }
 }
